3be6155c43494e8c4490d92989d9303f029b3cac
Shreyash <passionpagesshop@gmail.com>
2026-08-16 22:35:50 +0530

    feat: remove TopBar (address/phone/DTE/links header) from public layout

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom'
-import TopBar from './TopBar'
 import AnnouncementBar from './AnnouncementBar'
 import Navbar from './Navbar'
 import Footer from './Footer'
@@ -7,7 +6,6 @@ import Footer from './Footer'
 export default function PublicLayout() {
   return (
     <div className="min-h-screen flex flex-col">
-      <TopBar />
       <AnnouncementBar />
       <Navbar />
       <main className="flex-1">
